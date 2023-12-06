5bd161bd7db4bbc2589d98c1da51c57bfe6de954
Ishan Chawla <ishan.kpv@gmail.com>
2023-12-06 15:35:53 -0500

    format

--- practice-for-ch-js-intro-to-callbacks-exercises-long-practice-main/clock.js
+++ practice-for-ch-js-intro-to-callbacks-exercises-long-practice-main/clock.js
@@ -1,32 +1,32 @@
 class Clock {
-    constructor() {
-        let date = new Date()
-        this.hours = date.getHours()
-        this.minutes = date.getMinutes()
-        this.seconds = date.getSeconds()
+  constructor() {
+    let date = new Date();
+    this.hours = date.getHours();
+    this.minutes = date.getMinutes();
+    this.seconds = date.getSeconds();
 
-        this.printTime()
-        setTimeout(this._tick.bind(this), 1000)
+    this.printTime();
+    setTimeout(this._tick.bind(this), 1000);
         
-    }
-  
-    printTime() {
-        console.log(`${this.hours}; ${this.minutes}; ${this.seconds};`);
-    }
-  
-    _tick() {
-        this.seconds++
-        if (this.seconds < 60){
-            return;
-        }
-        this.seconds = 0
-        this.minutes++
-        if (this.minutes < 60){
-            return;
-        }
-        this.minutes = 0
-        this.hours++
-    }
   }
   
-  const clock = new Clock();
+  printTime() {
+    console.log(`${this.hours}; ${this.minutes}; ${this.seconds};`);
+  }
+  
+  _tick() {
+    this.seconds++;
+    if (this.seconds < 60){
+      return;
+    }
+    this.seconds = 0;
+    this.minutes++;
+    if (this.minutes < 60){
+      return;
+    }
+    this.minutes = 0;
+    this.hours++;
+  }
+}
+  
+const clock = new Clock();
--- practice-for-ch-js-intro-to-callbacks-exercises-long-practice-main/mybind.js
+++ practice-for-ch-js-intro-to-callbacks-exercises-long-practice-main/mybind.js
@@ -1,25 +1,25 @@
 Function.prototype.myBind = function (context){
-    return () => {
-        this.apply(context)
-    }
-}
+  return () => {
+    this.apply(context);
+  };
+};
 
 class Lamp {
-    constructor() {
-      this.name = "a lamp";
-    }
+  constructor() {
+    this.name = "a lamp";
   }
+}
   
-  const turnOn = function() {
-    console.log("Turning on " + this.name);
-  };
+const turnOn = function () {
+  console.log("Turning on " + this.name);
+};
   
-  const lamp = new Lamp();
+const lamp = new Lamp();
   
-  turnOn(); // should not work the way we want it to
+turnOn(); // should not work the way we want it to
   
-  const boundTurnOn = turnOn.bind(lamp);
-  const myBoundTurnOn = turnOn.myBind(lamp);
+const boundTurnOn = turnOn.bind(lamp);
+const myBoundTurnOn = turnOn.myBind(lamp);
   
-  boundTurnOn(); // should say "Turning on a lamp"
-  myBoundTurnOn(); // should say "Turning on a lamp"
+boundTurnOn(); // should say "Turning on a lamp"
+myBoundTurnOn(); // should say "Turning on a lamp"
