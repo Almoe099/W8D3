d0730dfb36da7ae88a0ca578d1d2a79e20cdec2b
Almoe099 <almoe9@LAPTOP-I8VP6L2K>
2023-12-06 15:26:13 -0500

    absurdbubblesort almost done

---
+++ practice-for-ch-js-intro-to-callbacks-exercises-long-practice-main/absurdbubblesort.js
@@ -0,0 +1,43 @@
+const readline = require('readline');
+let rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function askIfGreaterThan(el1, el2, callback){
+    rl.question(`Is ${el1} greater than ${el2}`, function (response) {
+        if (response === "yes"){
+            callback(true);
+        }else{
+            callback(false)
+        }
+    })
+};
+
+function innerBubbleSortLoop(arr, i, madeAnySwaps, outerBubbleSortLoop) {
+
+  if (i === arr.length - 1){
+    outerBubbleSortLoop(madeAnySwaps)
+  }else{
+    if (askIfGreaterThan(arr[i], arr[i + 1])) {
+      [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+      innerBubbleSortLoop(arr, i+1, true, outerBubbleSortLoop)
+    }
+  }
+}
+
+function absurdBubbleSort(arr, sortCompletionCallback) {
+  function outerBubbleSortLoop(madeAnySwaps) {
+    if (madeAnySwaps){
+      innerBubbleSortLoop(arr, 0, false, outerBubbleSortLoop)
+    }
+    sortCompletionCallback(arr)
+  }
+  outerBubbleSortLoop(true)
+}
+
+
+absurdBubbleSort([3, 2, 1], function(arr) {
+  console.log("Sorted array: " + JSON.stringify(arr));
+  rl.close();
+});
