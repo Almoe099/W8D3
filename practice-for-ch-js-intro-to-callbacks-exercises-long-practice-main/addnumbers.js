0cf13e45872fe2edbf5fb6c975224ac276a5df72
Almoe099 <almoe9@LAPTOP-I8VP6L2K>
2023-12-06 14:15:47 -0500

    completed addnumber

---
+++ practice-for-ch-js-intro-to-callbacks-exercises-long-practice-main/addnumbers.js
@@ -0,0 +1,24 @@
+const readline = require('readline');
+let rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function addNumbers(sum, numsLeft, completionCallback){
+    // console.log(numsLeft);
+    
+    if (numsLeft >= 1) {
+        rl.question("Give a me A Number", num => {
+            sum += parseInt(num)
+            console.log(sum);
+            if (numsLeft === 1) {
+                completionCallback(sum)
+                rl.close()
+                return;
+            }
+            addNumbers(sum, numsLeft-1, completionCallback)
+
+        })
+    }
+}
+addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
