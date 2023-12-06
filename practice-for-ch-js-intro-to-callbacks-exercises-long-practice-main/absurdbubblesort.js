9a391a5d2b255788fd4427f65cf882bce6916a18
Almoe099 <almoe9@LAPTOP-I8VP6L2K>
2023-12-06 17:04:46 -0500

    finshed absurdbubblesort

--- practice-for-ch-js-intro-to-callbacks-exercises-long-practice-main/absurdbubblesort.js
+++ practice-for-ch-js-intro-to-callbacks-exercises-long-practice-main/absurdbubblesort.js
@@ -1,5 +1,5 @@
 const readline = require("readline");
-let rl = readline.createInterface({
+const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
@@ -15,26 +15,29 @@ function askIfGreaterThan(el1, el2, callback) {
 }
 
 function innerBubbleSortLoop(arr, i, madeAnySwaps, outerBubbleSortLoop) {
-  if (i === arr.length - 1) {
+  if (i == arr.length - 1) {
     outerBubbleSortLoop(madeAnySwaps);
-  } else {
-    askIfGreaterThan(arr[i], arr[i + 1], (wasGreater) => {
-      if (wasGreater) {
-        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
-        innerBubbleSortLoop(arr, i + 1, true, outerBubbleSortLoop);
-      }
-    });
+    return;
   }
+  askIfGreaterThan(arr[i], arr[i + 1], (wasGreater) => {
+    if (wasGreater) {
+      debugger;
+      [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+      madeAnySwaps = true;
+    } 
+    innerBubbleSortLoop(arr, i + 1, madeAnySwaps, outerBubbleSortLoop);
+  });
 }
 
 function absurdBubbleSort(arr, sortCompletionCallback) {
   function outerBubbleSortLoop(madeAnySwaps) {
     if (madeAnySwaps) {
       innerBubbleSortLoop(arr, 0, false, outerBubbleSortLoop);
+    }else{
+      sortCompletionCallback(arr);
     }
   }
   outerBubbleSortLoop(true);
-  sortCompletionCallback(arr);
 }
 
 absurdBubbleSort([3, 2, 1], function (arr) {
